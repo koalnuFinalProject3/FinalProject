86e964e95e11893d70a4fc4b977e60e8b45f0835
Yongmin0423 <yym21345@gmail.com>
2025-04-30 20:33:12 +0900

    feat: 로그인 페이지 애니메이션 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import MainPage from "./pages/main-page/MainPage";
 import MyPage from "./pages/my-page/MyPage";
 import DiaryPage from "./pages/diary-page/DiaryPage";
 import TestPage from "./pages/test-page/TestPage";
+import Login from "./pages/login-page/Login";
 import "./App.css";
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<AppLayout />}>
-                <Route index element={<MainPage />} />
-                <Route path="my-page" element={<MyPage />} />
-                <Route path="diary-page" element={<DiaryPage />} />
-                <Route path="test-page" element={<TestPage />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="my-page" element={<MyPage />} />
+        <Route path="diary-page" element={<DiaryPage />} />
+        <Route path="test-page" element={<TestPage />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default App;
