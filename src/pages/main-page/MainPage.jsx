9637da69333e7c6b901b68e39a89169f54812773
Choi-jujuyeon <chlwndus1216@naver.com>
2025-05-01 00:39:56 +0900

    chore: modify Prettier config

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 // import styles from "./MyPage.module.css";
 
 const MainPage = () => {
-    return (
-        <div>
-            <h1>MainPage</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>MainPage</h1>
+    </div>
+  );
 };
 
 export default MainPage;
