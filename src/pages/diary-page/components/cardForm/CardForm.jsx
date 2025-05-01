ce5ad6d036debc23c5faa6188f264ac35a5b0713
Choi-jujuyeon <chlwndus1216@naver.com>
2025-05-01 13:10:34 +0900

    feat: Create CardForm component and apply layout

--- src/pages/diary-page/components/cardForm/CardForm.jsx
+++ src/pages/diary-page/components/cardForm/CardForm.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import styles from './CardForm.module.css';
+import arrowIcon from '../../../../assets/icons/caretCircleRight.svg';
+
 const CardForm = () => {
-  return <div className={styles.container}>카드 폼 영역입니다</div>;
+  return (
+    <div className={styles.cardBox}>
+      <div className={styles.cardAvatar}>여기에 감정이모지</div>
+      <div className={styles.cardContent}>
+        <div className={styles.cardDate}>2023. 04. 29</div>
+        <div className={styles.cardIndicator}>
+          <span className={styles.text}>여기는 내용을 적는 부분</span>
+
+          <img className={styles.arrow} src={arrowIcon} alt="arrow" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardForm;
--- src/pages/diary-page/components/cardListBar/CardListBar.jsx
+++ src/pages/diary-page/components/cardListBar/CardListBar.jsx
@@ -4,11 +4,22 @@ import CardForm from '../cardForm/CardForm';
 const CardListBar = () => {
   return (
     <div className={styles.container}>
+      <h1>다이어리 햄버거</h1>
+      <h1>다이어리 필터</h1>
+      <h1>다이어리 카드</h1>
       <div className={styles.innerContainer}>
-        <h1>다이어리 햄버거</h1>
-        <h1>다이어리 필터</h1>
-        <h1>다이어리 카드</h1>
-
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
+        <CardForm />
         <CardForm />
       </div>
     </div>
