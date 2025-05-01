66d6afb8a86095a5b28c35ee7fc92720b470ffc5
Choi-jujuyeon <chlwndus1216@naver.com>
2025-05-01 21:46:26 +0900

    feat: Add shared emoji images assets

--- src/layout/SideNav/SideNav.jsx
+++ src/layout/SideNav/SideNav.jsx
@@ -1,53 +1,56 @@
 import React from 'react';
 import styles from './SideNav.module.css';
 import logo from '../../assets/images/FeelPiece.png';
-import profile from '../../assets/images/profile.png';
+import profile from '../../assets/images/happyChar.png';
 import user from '../../assets/icons/user.svg';
 import diary from '../../assets/icons/diary.svg';
-import setting from '../../assets/icons/Settings.svg'
+import setting from '../../assets/icons/Settings.svg';
 import { Link } from 'react-router-dom';
 
 const SideNav = () => {
   return (
     <div className={styles.sideNavArea}>
-        <div className={styles.sideNavInner}>
-            {/* 로고 이름 */}
-            <Link to='/' className={`${styles.sideNavTitle}`}>
-                <img src={logo} alt='feel-piece-logo' />
-                느낌한조각
-            </Link>
+      <div className={styles.sideNavInner}>
+        {/* 로고 이름 */}
+        <Link to="/" className={`${styles.sideNavTitle}`}>
+          <img src={logo} alt="feel-piece-logo" />
+          느낌한조각
+        </Link>
 
-            {/* 프로필 */}
-            <div className={`${styles.navProfileImageArea} ${styles.acCenter}`}>
-                <img src={profile} alt='profile-image' />
-                <div>OOO 님</div>
-            </div>
-
-            {/* 버튼 */}
-            <div className={styles.btnArea}>
-                <Link to='/my-page' className={styles.aCenter}>
-                    <img src={user} alt='user' /><span>내 정보</span>
-                </Link>
-                <Link to='/diary-page' className={styles.aCenter}>
-                    <img src={diary} alt='diary' /><span>내 일기</span>
-                </Link>
-                <Link to='/test-page' className={styles.aCenter}>
-                    <img src={setting} alt='setting' /><span>설정하기</span>
-                </Link>
-            </div>
-
-            {/* footer */}
-            <div className={`${styles.footer}`}>
-                <div>이 프로젝트는 5명의 조각들이 함께 만든 감정의 기록입니다. </div>
-                <div>
-                    <div>Team Members:</div>
-                    <div>안연섭, 유용민, 정다원, 정이레, 최주연</div>
-                </div>
-                <div className={`${styles.footerName}`}>ⓒ 2025 느낌한조각</div>
-            </div>
+        {/* 프로필 */}
+        <div className={`${styles.navProfileImageArea} ${styles.acCenter}`}>
+          <img src={profile} alt="profile-image" />
+          <div>OOO 님</div>
         </div>
-    </div>
-  )
-}
 
-export default SideNav
+        {/* 버튼 */}
+        <div className={styles.btnArea}>
+          <Link to="my" className={styles.aCenter}>
+            <img src={user} alt="user" />
+            <span>내 정보</span>
+          </Link>
+          <Link to="diary" className={styles.aCenter}>
+            <img src={diary} alt="diary" />
+            <span>내 일기</span>
+          </Link>
+          <Link to="test" className={styles.aCenter}>
+            <img src={setting} alt="setting" />
+            <span>설정하기</span>
+          </Link>
+        </div>
+
+        {/* footer */}
+        <div className={`${styles.footer}`}>
+          <div>이 프로젝트는 5명의 조각들이 함께 만든 감정의 기록입니다. </div>
+          <div>
+            <div>Team Members:</div>
+            <div>안연섭, 유용민, 정다원, 정이레, 최주연</div>
+          </div>
+          <div className={`${styles.footerName}`}>ⓒ 2025 느낌한조각</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
