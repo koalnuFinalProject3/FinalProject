cd888c3d8cc8abda53a7c8be17c55398e5119f42
lliwIWant <liwil5486@gmail.com>
2025-05-04 04:25:37 +0900

    달력 스타일 변경

--- src/pages/main-page/components/CalendarDiary/CalendarDiary.jsx
+++ src/pages/main-page/components/CalendarDiary/CalendarDiary.jsx
@@ -278,6 +278,7 @@ const CalendarDiary = ({ setCalendarType }) => {
         }))}
         eventContent={renderEventContent}
         dateClick={handleDateClick}
+        eventClick={handleDateClick}
         droppable={true}
         drop={handleDrop}
         /* －－－－－－－－－－－－－－ */
